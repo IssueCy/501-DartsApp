d770e8e495c24d02cb9d8e83ad5db1e6720c11a2
IssueCy <simtecapplications@web.de>
2024-12-29 13:50:43 +0100

    Added display logic for index.html

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
+let players = {
+    "player1" : "merix"
+};
+
+window.onload = () => {
+    if (!localStorage.getItem("players")) {
+        localStorage.setItem("players", players)
+    } else {
+        localStorage.getItem("players").split() //TODO load players in ls and in dom
+    }
+};
+
 function quitApp() {
     window.close();
 }
 
-let players_container = document.getElementById('players-container');
+let addPlayer_button = document.getElementById('addPlayer_button');
 let create_player_container = document.getElementById('createPlayersContainer');
-document.getElementById('addPlayer_button').addEventListener('click', () => {
+let players_container = document.getElementById('players-container');
+let confirmAddPlayer_button = document.getElementById('confirmAddPlayer_button');
 
-});
+if (addPlayer_button) {
+    addPlayer_button.addEventListener('click', () => {
+        addPlayer_button.style.display = "none";
+        create_player_container.style.display = "flex";
+    });
+}
+
+if (confirmAddPlayer_button) {
+    function confirmAddPlayer() {
+        //TODO      - localstorage  - displays
+        let inputField = document.getElementById('name_input_field');
+        let newName = inputField.value;
+
+        
+    }
+}
 
 function startGame() {
 
